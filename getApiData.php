2d85c1f1b7b486bd42581771913af29a91abac75
Genadi <genadi.kozarev@gmail.com>
2021-09-14 15:55:15 +0100

    database populated with duplicates

--- getApiData.php
+++ getApiData.php
@@ -3,15 +3,11 @@
 $request = curl_init("https://www.freetogame.com/api/games?platform=pc");
 curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($request);
-$data = json_decode($response);
-
-print_r($data);
+$data = json_decode($response, true);
 
 $db = new PDO('mysql:host=127.0.0.1; dbname=games', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('INSERT INTO `pc-games` (`title`, `thumbnail`, `shortDescription`, `gameUrl`, `genre`, `platform`, `publisher`, `developer`, `releaseDate`, `freetogameProfileUrl`) VALUES (:title, :thumbnail, :shortDescription, :gameUrl, :genre, :platform, :publisher, :developer, :releaseDate, :freetogameProfileUrl);');
-
-
 
 foreach ($data as $game ) {
     $query->bindParam(':title', $game['title']);
