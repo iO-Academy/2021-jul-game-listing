fdff6abb616da2b9c41895a108a6200b5e0cb213
Rosa <rosa@droogers.nl>
2021-09-16 10:49:54 +0100

    added html and classes

--- index.php
+++ index.php
@@ -7,7 +7,32 @@ use GameListing\ViewHelpers\GameViewHelper;
 $db = new PDO('mysql:host=db;dbname=games', 'root', 'password');
 $games = GamesHydrator::getAllGames($db);
 
-foreach($games as $game) {
-  echo GameViewHelper::createGameCard($game);
+?>
 
-}
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <title>Game Listing</title>
+    <link rel="stylesheet" href="https://use.typekit.net/mpi6fsb.css">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <a href="#"><img src=""></a>
+    </header>
+    <main id="homePage">
+        <h1>Game Listing</h1>
+        <div class="cardContainer">
+            <?php
+                foreach($games as $game) {
+                    echo GameViewHelper::createGameCard($game);
+                }
+            ?>
+        </div>
+    </main>
+    <footer>
+        <a href="#homePage">back to top</a>
+    </footer>
+</body>
+</html>
